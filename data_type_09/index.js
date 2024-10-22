254406bac201b9f43288298859af835c6f864622
sumittripati <99213688+sumittripati@users.noreply.github.com>
2024-10-22 18:46:15 +0000

    data_types

---
+++ data_type_09/index.js
@@ -0,0 +1,45 @@
+// Premitive
+
+// 7 Types => String , Number , Boolean , Null , Symbol , Undefined , BigInt
+
+// Reference (Non Primitive)
+
+// Array , Object , Function
+
+
+
+// let num = null
+// console.log(num)
+// console.log(typeof num)
+
+// let num = undefined
+// console.log(num)
+// console.log(typeof num)
+
+// let a = Symbol('wer')
+// console.log(typeof a)
+// console.log(a)
+// let id = Symbol('wer')
+// console.log(typeof id)
+// console.log(id)
+// console.log(a === id)
+
+// let b = 234234234234n
+// console.log(typeof b)
+
+// let a = ["sumit", "tripathi"]
+// console.log(typeof a)
+
+// let a = [22, 223]
+// console.log(typeof a)
+
+// let sum = {
+//     name : "sumit",
+//     age : 20
+// }
+// console.log(typeof sum)
+
+let myfun = function () {
+    console.log("hii")
+}
+console.log(typeof myfun)
