dfece0d7c2a424e6b1925d87f205c3acb76782f6
sumittripati <99213688+sumittripati@users.noreply.github.com>
2024-10-22 17:59:37 +0000

    type_convertions

---
+++ type_convertion_06/index.js
@@ -0,0 +1,41 @@
+// let a = 33
+// console.log(typeof a)
+
+// let a = "44"
+// console.log(typeof a)
+
+// let a = "44"
+// let num = Number(a)
+// console.log (typeof num)
+
+//  let a = "44aa"
+// console.log(a)
+// console.log(typeof a)
+
+// let a = "345aaa"
+// let num = Number(a)
+// console.log(typeof num)
+// console.log(num)
+
+// let b = null
+// let c = Number(b)
+// console.log(c)
+
+// let b = undefined
+// let c = Number(b)
+// console.log(c)
+
+// "8787jjhbj"  => type of = NaN  in convertion
+
+// let a = "sumit"
+// let b = Boolean(a)
+// console.log(b)                       // true
+
+// let a = ""
+// let b = Boolean(a)
+// console.log(b)                                  //false
+
+let a = 33
+let b = String(a)
+console.log(b)
+console.log(typeof b)
