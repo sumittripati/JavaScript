1c5239bbd8bb4d4596a2e4313b92a05f804f02c2
sumittripati <99213688+sumittripati@users.noreply.github.com>
2024-10-23 11:51:17 +0000

    done

---
+++ lec12/nunber_and_math_12.js
@@ -0,0 +1,39 @@
+// let num = new Number(100)
+// console.log(num)
+
+let num = new Number(100)
+console.log(num.toString().length)
+
+   //    let num = new Number(100)
+    //    console.log(num)
+
+// let num = new Number(100)
+// console.log(num.toString())
+
+// let num = new Number(100)
+// console.log(num.toString().length)
+
+// let num = new Number(100.54)
+// console.log(num.toFixed(1))
+
+// let num = new Number(10000000)
+// // console.log(num.toLocaleString())
+// console.log(num.toLocaleString('en-IN'))
+
+
+
+// console.log(Math)
+// console.log(Math.abs(-6))
+// console.log(Math.round(7566.7))
+// console.log(Math.ceil(65.3))
+// console.log(Math.floor(65.3))
+// console.log(Math.min(2,5,1))
+// console.log(Math.max(2,5,1))
+
+console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor((Math.random()*10) + 1))
+
+let min = 10
+let max = 20
+console.log(Math.floor((Math.random() * (max - min +1)) + min))
---
+++ string_11.js
@@ -0,0 +1,34 @@
+// let name = new String ("sumit")
+// console.log(name)
+
+// let name = new String ("sumit")
+// console.log(name.length)
+// console.log(name.__proto__)
+// console.log(name.charAt(2))
+// console.log(name.indexOf('m'))
+
+// let name = new String ("sumit")
+// let myname = name.substring(1,4)
+// console.log(myname)
+
+// let name = new String ("sumit")
+// let myname = name.slice(-2,4)
+// console.log(myname)
+
+// let name = new String ("   sumit   ")
+// console.log(name.trim())
+
+// let name = new String ("sumit")
+// console.log(name.replace('um', 'tr'))
+
+// let name = new String ("sumit")
+// console.log(name.includes('um'))
+
+// let name = new String ("sumit-tripathi")
+// console.log(name.split('-'))
+
+// let name = new String ("sumit tripathi")
+// console.log(name.split(' '))
+
+let name = new String ("sumit tripathi")
+console.log(name.split(''))
